79d74aa6bb8723917b44c7f7720146ad3fcf90d3
JosiMacGyver <joseana.stos.soares@gmail.com>
2023-01-25 16:10:00 -0300

    actions

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -3,32 +3,52 @@ import React from 'react'
 
 import Card from './Card'
 import { connect } from 'react-redux'
+import { alterarNumeroMaximo, alterarNumeroMinimo } from '../store/actions/numeros'
 
 function Intervalo(props) {
 
-    const {min, max} = props
+        const { min, max } = props
 
-    return (
-        <Card title="Intervalo de Números" red>
-            <div className='Intervalo'>
-                <span>
-                    <strong>Mínimo:</strong>
-                    <input type="number" value={min}/>
-                </span>
-                <span>
-                    <strong>Máximo:</strong>
-                    <input type="number" value={max}/>
-                </span>
-            </div>
-        </Card>
-    )
-}
+        return (
+            <Card title="Intervalo de Números" red>
+                <div className='Intervalo'>
+                    <span>
+                        <strong>Mínimo:</strong>
+                        <input type="number" value={min} 
+                        onChange={e => props.alteraMinimo(+e.target.value)}/>
+                    </span>
+                    <span>
+                        <strong>Máximo:</strong>
+                        <input type="number" value={max} 
+                        onChange={e => props.alteraMaximo(+e.target.value)}/>
+                    </span>
+                </div>
+            </Card>
+        )
+    }
 
 function mapStateToProps(state) {
-    return{
-        min:state.numeros.min,
-        max:state.numeros.max,
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max,
     }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+function mapDispatchToProps(dispatch){
+    return{
+        alteraMinimo(novoNumero) {
+            //action creator ->
+            const action = alterarNumeroMinimo(novoNumero);
+            dispatch(action);
+        },
+        alteraMaximo(novoNumero) {
+            const action = alterarNumeroMaximo(novoNumero);
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Intervalo)
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,17 +2,33 @@ import { legacy_createStore as createStore } from "redux";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
-    numeros: function(state, action){
-        //console.log(state, ' ', action)
-        return{
-            min:7,
-            max:31
+    numeros: function (state, action) {
+        console.log('Reducer de Números')
+        console.log(state, ' ', action)
+
+        switch (action.type) {
+            case 'NUM_MIN_ALTERADO':
+                return {
+                    ...state,
+                    min: action.payload
+                }
+            case 'NUM_MAX_ALTERADO':
+                return{
+                    ...state,
+                    max: action.payload
+                }
+            default:
+                return {
+                    min: 7,
+                    max: 31
+                }
         }
     },
 
-    nomes: function(state, action) {
-        //console.log(state, '', action)
-        return[
+    nomes: function (state, action) {
+        console.log('Reducer de Nomes')
+        console.log(state, '', action)
+        return [
             'Ana',
             'Bia',
             'Carlos'
