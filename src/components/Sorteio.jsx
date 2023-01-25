4f7fdf768b7fbea408d896b1840cf5cd7d6e5c71
JosiMacGyver <joseana.stos.soares@gmail.com>
2023-01-25 15:12:04 -0300

    passando estado inicial

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Card from './Card'
+import { connect } from 'react-redux'
 
-export default props => {
+const Sorteio = props => {
+
+    const { min, max } = props
+    const aleatorio = parseInt(Math.random() * (max - min) + min)
 
     return (
         <Card title="Sorteio de um Números" purple>
             <div className='Intervalo'>
                 <span>
                     <span>Resultado: </span>
-                    <strong>5</strong>
+                    <strong>{aleatorio}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max,
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio)
